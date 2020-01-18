ddcfc9e1c4dadbc86a1054a0fc1b5a0453e49f67
Bruno Martins <bgcngm@gmail.com>
2020-01-18 19:04:13 +0000

    lmi: Import devicesettings
     * Base off LineageOS/android_device_xiaomi_sdm845-common.
    
    Change-Id: I0d461134e1a2513ad410d46527e7d7eb968d2ba9

--- device.mk
+++ device.mk
@@ -107,6 +107,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=true
 
+# Device parts
+PRODUCT_PACKAGES += \
+    DeviceParts
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
