c269e9c00f05ed182b59072eb956879cf08c671f
TH779 <tongsj2333@gmail.com>
2020-12-10 08:42:32 +0800

    lmi: Import power HAL from LA.UM.8.12.r1-14400-sm8250.0
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Change-Id: Idadfea60080ef6917dec0ae93ca82b3c6320eafa
    Reviewed-on: https://mokeedev.review/c/MoKee/android_device_xiaomi_lmi/+/49761

--- device.mk
+++ device.mk
@@ -170,6 +170,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power.stats@1.0-service.mock
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.2-service.lmi
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
