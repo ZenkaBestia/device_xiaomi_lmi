2e5ff70f05f21034b09585ec3600179ff420471c
dataoutputstream <mb150996@gmail.com>
2020-12-28 19:33:01 +0100

    lmi: Sepolicy: Clean up and re-format

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,10 +178,11 @@ TARGET_USES_MKE2FS := true
 
 # Sepolicy
 SELINUX_IGNORE_NEVERALLOWS := true
-
+include device/qcom/sepolicy/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private-vendor
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-
 
 # System as root
 BOARD_SUPPRESS_SECURE_ERASE := true
