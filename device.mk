80078323209928f9ddfd5b4112ebd3c37d121b2a
ZenkaBestia <z.mail.6600@gmail.com>
2021-02-05 22:33:37 +0000

    lmi: remove defined MODULE.TARGET.APPS.Bluetooth

--- device.mk
+++ device.mk
@@ -184,8 +184,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     vendor/nxp/opensource/sn100x\
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth\
-        vendor/qcom/opensource/commonsys/system/bt/conf
             
 # Telephony
 PRODUCT_PACKAGES += \
