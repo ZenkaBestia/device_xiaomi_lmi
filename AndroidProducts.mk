80f69a9c900cfc281dcd4969185e3a250e9635db
ZenkaBestia <z.mail.6600@gmail.com>
2021-02-06 02:42:39 +0000

    lmi: Superior-ify

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/superior_lmi.mk
+
+COMMON_LUNCH_CHOICES := \
+    superior_lmi-user \
+    superior_lmi-userdebug \
+    superior_lmi-eng
