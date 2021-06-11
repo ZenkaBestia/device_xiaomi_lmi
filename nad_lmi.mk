b6ae77cc34c4cf322c7a2dd4304c9b30d5fe3768
ZenkaBestia <z.mail.6600@gmail.com>
2021-06-11 11:59:48 +0000

    lmi: Cinta Nusantara

--- nad_lmi.mk
+++ nad_lmi.mk
@@ -8,13 +8,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Nusantara stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-PRODUCT_NAME := lineage_lmi
+# Inherit Nusantara build
+NAD_BUILD_TYPE := OFFICIAL
+USE_GAPPS ?= true
+USE_AOSP_CLOCK := true
+USE_PIXEL_CHARGING := true
+TARGET_USES_BLUR := true
+TARGET_FOD_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := nad_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
