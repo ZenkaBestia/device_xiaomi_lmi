49b7c96fe2c135d4702a4394bbc0eb5f21882a9d
ZenkaBestia <z.mail.6600@gmail.com>
2021-10-03 05:29:36 +0000

    lmi: Adapt to Legion OS and make it Official

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lmi.mk
+    $(LOCAL_DIR)/legion_lmi.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lmi-userdebug \
-    lineage_lmi-eng
+    legion_lmi-userdebug \
+    legion_lmi-eng
--- legion_lmi.mk
+++ legion_lmi.mk
@@ -11,11 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Legion OS stuff.
+$(call inherit-product, vendor/legion/config/common_full_phone.mk)
+
+# Legion OS Build
+LEGION_MAINTAINER := 家|Zenka
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.maintainer.name=家|Zenka
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_BLUR := true
+TARGET_SUPPORT_FACE_UNLOCK := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_lmi
+PRODUCT_NAME := legion_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30 Pro
